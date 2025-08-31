8fd6d8106c635ff9123e0eec513583c2b01faa05
AryoKM <121080759+Oreo-1@users.noreply.github.com>
2025-08-31 19:29:50 +0800

    playing around some w/ frameworks

---
+++ assets/main.js
@@ -0,0 +1,31 @@
+// script for the visuals in hero section
+// VANTA.NET({
+//   el: "#vanta-bg",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.0,
+//   scaleMobile: 1.0,
+//   color: 0x00ffff,
+//   backgroundColor: 0x111111
+// });
+
+// catppuccin colors
+// base: #24273a
+// blue: #8aadf4
+
+// testing version
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x8aadf4,
+  backgroundColor: 0x181926
+});
